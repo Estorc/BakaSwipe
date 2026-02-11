70689adf311a750cb0d9f9bdd9727982d0adab06
ceb <lavallard.cebastien@gmail.com>
2026-02-11 14:53:05 +0100

    page listAnime

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { SwipeComponent } from '../swipe/swipe.component';
+import { ListAnimeComponent } from './list-anime/list-anime.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'swipe', component: SwipeComponent },
+  { path: 'list', component: ListAnimeComponent},
   { path: '**', redirectTo: '/login' }
 ];
